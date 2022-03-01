64e7ebf62a391c8a92c3e54de5b076bf128bbe56
AlexShepelSwift <76876025+AlexShepelSwift@users.noreply.github.com>
2022-03-01 20:47:04 +0300

    Initial Commit

---
+++ trafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLights
+//
+//  Created by Александр Шепель on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
