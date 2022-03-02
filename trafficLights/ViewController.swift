8712fe32e8470cfd779aeeefbb28d2e79ffc3c0e
AlexShepelSwift <76876025+AlexShepelSwift@users.noreply.github.com>
2022-03-02 18:25:10 +0300

    startConfiguration

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLight.layer.cornerRadius = redLight.layer.bounds.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.layer.bounds.width / 2
+        greenLight.layer.cornerRadius = greenLight.layer.bounds.width / 2
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
